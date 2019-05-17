970369a560214ef2ca994b18494f048b9b2f11c7
Damir <damir.aljovic@msgconsulting.net>
2019-05-17 22:12:32 +0200

    update server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,9 +10,42 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+  serverStatus: string;
+  isLoading: boolean;
   @Input() serverInput: Server;
 
   ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.serverStatus = 'Online';
+      this.color = '#66BB6A',
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.serverStatus = 'Offline';
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+
+  makeLoading() {
+    this.color = '#FFCA28';
+    this.buttonText = 'Pending...';
+    this.isLoading = true;
+    this.serverStatus = 'Loading';
+  }
+
+  sendServerAction(isOnline: boolean) {
+    console.log('sendServerAction called!');
+    this.makeLoading();
+    //const payload = this.buildPayload(isOnline);
+    //this.serverAction.emit(payload);
   }
 
 }
