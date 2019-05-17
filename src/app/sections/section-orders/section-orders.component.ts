1d8856539123d6eb8c0b0f3fcea1b2063941ccea
Damir <damir.aljovic@msgconsulting.net>
2019-05-17 22:36:43 +0200

    order component

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -10,6 +11,41 @@ export class SectionOrdersComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.getOrders();
+  }
+
+  orders: Order[];
+  total = 0;
+  page = 1;
+  limit = 10;
+  loading = false;
+
+  
+  getOrders(): void {
+    /*this._salesData.getOrders(this.page, this.limit)
+      .subscribe(res => {
+        console.log('Result from getOrders: ', res);
+        this.orders = res['page']['data'];
+        this.total = res['page'].total;
+        this.loading = false;
+      }); */
+  }
+
+  goToPrevious(): void {
+    // console.log('Previous Button Clicked!');
+    this.page--;
+    this.getOrders();
+  }
+
+  goToNext(): void {
+    // console.log('Next Button Clicked!');
+    this.page++;
+    this.getOrders();
+  }
+
+  goToPage(n: number): void {
+    this.page = n;
+    this.getOrders();
   }
 
 }
